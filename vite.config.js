70f7b600a1478b0000519125387e077b82525921
eureves <eureves@gmail.com>
2023-10-02 16:44:13 +0300

    17.06

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,43 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      injectRegister: 'auto',
+      manifest: {
+        name: 'Musisch',
+        theme_color: '#ff5e3a',
+        icons: [
+          {
+            src: 'assets/img/pwa-192x192.png',
+            sizes: '192x192',
+            mime: 'image/png'
+          }
+        ]
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,png,jpg}']
+      }
+    })
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {
+    rollupOptions: {
+      external: ['workbox-window']
     }
   }
 })
