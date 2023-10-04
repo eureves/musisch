5a986743367af8660914fa420be689277fc410cb
eureves <eureves@gmail.com>
2023-10-04 13:59:07 +0300

    20.05

---
+++ src/components/__tests__/aboutView.spec.js
@@ -0,0 +1,8 @@
+import About from '@/views/AboutView.vue'
+import { mount } from '@vue/test-utils'
+
+describe('AboutView.vue', () => {
+  it('Renders inner text', () => {
+    const wrapper = mount(About)
+  })
+})
